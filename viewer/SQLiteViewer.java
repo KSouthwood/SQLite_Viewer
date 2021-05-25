eb246ac37abc0636c286c948eeda538c8d0d1862
Keri Southwood-Smith <9258461+KSouthwood@users.noreply.github.com>
2021-05-24 20:36:52 -0400

    Complete Stage 3 (What's inside?)
    Add a JTable component to display the data from a selected database table.
    
    On clicking the execute button in the window, run the selected query on the table, read all the data in and display it in the table.
    
    SQLiteViewer.java:
    - Move all instantiations of components to be field instances of the class
    - Add action listener for the execute button

--- viewer/SQLiteViewer.java
+++ viewer/SQLiteViewer.java
@@ -1,9 +1,22 @@
 package viewer;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.sql.*;
+import java.util.Vector;
 
 public class SQLiteViewer extends JFrame {
+    JTextField filename = new JTextField();
+    JButton openButton = new JButton("Open");
+    JComboBox<String> tableComboBox = new JComboBox<>();
+    JTextArea queryText = new JTextArea();
+    JButton executeButton = new JButton("Execute");
+
+    Vector<Object> columnNames = new Vector<>();
+    Vector<Vector<Object>> tableData = new Vector<>();
+    DefaultTableModel model = new DefaultTableModel(tableData, columnNames);
+    JTable table = new JTable(model);
+    JScrollPane scrollPane = new JScrollPane(table);
 
     public SQLiteViewer() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,35 +32,36 @@ public class SQLiteViewer extends JFrame {
     }
 
     void init() {
-        // filename text field
-        JTextField filename = new JTextField();
+        // filename text field properties
         filename.setBounds(20, 10, 540, 20);
         filename.setName("FileNameTextField");
         add(filename);
 
-        JButton openButton = new JButton("Open");
+        // open button properties
         openButton.setBounds(580, 10, 100, 20);
         openButton.setName("OpenFileButton");
         add(openButton);
 
 
-        // list of tables
-        JComboBox<String> tableComboBox = new JComboBox<>();
+        // combo box properties (holds list of tables)
         tableComboBox.setBounds(20, 50, 660, 20);
         tableComboBox.setName("TablesComboBox");
         add(tableComboBox);
 
-        // text area for queries
-        JTextArea queryText = new JTextArea();
-        queryText.setBounds(20, 90, 660, 400);
+        // text area for queries properties
+        queryText.setBounds(20, 90, 660, 200);
         queryText.setName("QueryTextArea");
         add(queryText);
 
-        // execute button
-        JButton executeButton = new JButton("Execute");
-        executeButton.setBounds(20, 510, 100, 20);
+        // execute button properties
+        executeButton.setBounds(580, 310, 100, 20);
         executeButton.setName("ExecuteQueryButton");
         add(executeButton);
+
+        // table properties
+        table.setName("Table");
+        scrollPane.setBounds(20, 350, 660, 400);
+        add(scrollPane);
 
         openButton.addActionListener(e -> {
             Connection conn;
@@ -66,8 +80,40 @@ public class SQLiteViewer extends JFrame {
             }
         });
 
-        tableComboBox.addActionListener(e -> {
-            queryText.setText("SELECT * FROM " + tableComboBox.getSelectedItem() + ";");
+        tableComboBox.addActionListener(e -> queryText.setText("SELECT * FROM " + tableComboBox.getSelectedItem() + ";"));
+
+        executeButton.addActionListener(e -> {
+            Connection conn;
+            columnNames.clear();
+            tableData.clear();
+            try {
+                String url = "jdbc:sqlite:" + filename.getText().trim();
+                conn = DriverManager.getConnection(url);
+                Statement statement = conn.createStatement();
+                ResultSet resultSet =
+                        statement.executeQuery(queryText.getText());
+                ResultSetMetaData metaData = resultSet.getMetaData();
+
+                // get number of columns and read the names
+                int columnCount = metaData.getColumnCount();
+                if (columnCount > 0) {
+                    for (int col = 1; col <= columnCount; col++) {
+                        columnNames.add(metaData.getColumnName(col));
+                    }
+                    model.setColumnIdentifiers(columnNames);
+                }
+
+                // read the data row by row
+                while (resultSet.next()) {
+                    Vector<Object> rowData = new Vector<>();
+                    for (int col = 1; col <= columnCount; col++) {
+                        rowData.add(resultSet.getString(col));
+                    }
+                    tableData.add(rowData);
+                }
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
         });
     }
 }
